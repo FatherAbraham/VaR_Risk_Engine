67c91cbbd000a2d8631b5c88e8edd4003f59f781
FatherAbraham <damianspendel@Damians-MacBook-Air.local>
2014-09-27 16:59:39 +0200

    updated

--- server.R
+++ server.R
@@ -22,20 +22,4 @@ shinyServer(function(input, output){
    
   })
 
-  output$TimeSeries<-renderPlot({
-    begin <- Sys.Date() - 252 ## doesn't actually get 252 entries so the logic here needs improving
-    end <- Sys.Date()   ## we can just use the last(dataset, 252) function, once we have a large enough dataset
-    
-    ##next few lines can probably be optimized?
-    
-    tickers <- getSymbols(input$Instrument, from = begin, to = end, auto.assign = TRUE)
-    
-    dataset <- Ad(get(tickers[1]))
-    dataset <- dataset[,1]
-    returns <- Return.calculate(dataset, method=c("simple"))
-    
-
-    chartSeries(returns)
-  })
-  
-})  
+})
